998c97edd92e7cd96d7f4991fdd5d6fd9f8f1c3f
ryarlagadda <ryarlag2@uncc.edu>
2016-07-26 23:19:26 -0400

    Create DbUtil.java

---
+++ Util/DbUtil.java
@@ -0,0 +1,39 @@
+package Util;
+
+import java.sql.*;
+
+public class DbUtil {
+
+    
+    public static void closePreparedStatement(Statement ps) {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+    
+    public static void closeStatement(Statement s) {
+        try {
+            if (s != null) {
+                s.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+    
+    
+}
